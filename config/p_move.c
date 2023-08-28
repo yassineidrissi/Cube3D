1c6409e61560b620b2e246f425a8001fb3112295
Yassine Idrissi <yaidriss@c1r6p1.1337.ma>
2023-08-28 12:02:05 +0100

    im not sure

--- config/p_move.c
+++ config/p_move.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   p_move.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zouaraqa <zouaraqa@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 23:57:48 by yaidriss          #+#    #+#             */
-/*   Updated: 2023/08/27 13:18:23 by zouaraqa         ###   ########.fr       */
+/*   Updated: 2023/08/27 16:56:34 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,27 @@
 int is_wall_pixel(t_cub3D *cb, float x, float y)
 {
 	// printf("%f %f\n",x, y);// it segfault when it goes down
-	if (cb->map.map_tmp[(int)y / COF_PIXEL][(int)x / COF_PIXEL] == '1')
+	// if ( y > cb->map.height)
+	// {
+	// 	printf("we out of maps\n");
+	// 	return (0);
+	// }
+	// }
+	int pos_y;
+	int pos_x;
+
+	pos_y = (int)y / COF_PIXEL;
+	pos_x = (int)x / COF_PIXEL;
+	if (pos_x < 0 || pos_y < 0 || pos_x > cb->map.width || pos_y > cb->map.height)
+	{
+		printf("we out of maps and x is %d and y is %d\n", pos_x, pos_y);		
 		return (0);
+	}
+	if (cb->map.map_tmp[pos_y][pos_x] == '1')
+	{
+		printf("the height is %d and the wiedth is %d and x is %d and y is %d\n", cb->map.height,cb->map.width,pos_x,pos_y);	
+		return (0);
+	}
 	return (1);
 }
 
@@ -75,17 +94,37 @@ int is_wall_pixel(t_cub3D *cb, float x, float y)
 // 	// printf ("[%f]\n", ray_dis);
 // }
 
+
+void ft_calculate_distance_h(t_cub3D *cb)
+{
+	float x_ray;
+	float y_ray;
+	float next_x;
+	int i = 1;
+	float next_y;
+
+	next_x = (int)(cb->player.x / COF_PIXEL) * COF_PIXEL + COF_PIXEL*(i++);
+	next_y = cb->player.y - next_x * tan(cb->angle * M_PI / 180);
+	while (is_wall_pixel(cb, next_x, next_y))
+	{
+		next_x = (int)(cb->player.x / COF_PIXEL) * COF_PIXEL + COF_PIXEL*(i++);
+		next_y = cb->player.y - next_x * tan(cb->angle * M_PI / 180);
+	}
+	printf("the position of the wall is %d %d angle %d\n", (int)(next_x/COF_PIXEL), (int)next_y/COF_PIXEL,cb->angle);
+}
+
 //! this function is used to change player angle
 void change_angle(t_cub3D *cb, int KEY)
 {
-	if (KEY == MLX_KEY_RIGHT && (cb->angle)%360 <= 170)
-		cb->angle = (cb->angle + 10) % 350;
-	else if (KEY ==  MLX_KEY_LEFT && cb->angle >= -170)
+	if (KEY == MLX_KEY_RIGHT && (cb->angle) > 0)
 		cb->angle = (cb->angle - 10) % 350;
-	else if (cb->angle == 180 && KEY == MLX_KEY_RIGHT)
-		cb->angle = -170;
+	else if (KEY ==  MLX_KEY_LEFT && cb->angle < 350)
+		cb->angle = (cb->angle + 10) % 350;
+	else if (cb->angle == 0 && KEY == MLX_KEY_RIGHT)
+		cb->angle = 350;
 	else
-		cb->angle = 170;
+		cb->angle = 0;
+	ft_calculate_distance_h(cb);
 }
 
 //! this function to change player position
@@ -141,6 +180,7 @@ void change_player(t_cub3D *cb, int KEY)
 		// printf("the angle is %d in a\n", cb->angle);
 		// printf("the map is %c\n",cb->map.map_tmp[(int)next_y/COF_PIXEL][(int)next_x/COF_PIXEL]);
 	}
+	ft_calculate_distance_h(cb);
 }
 
 //! this function for key hook so every key do action functionality
