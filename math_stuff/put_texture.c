e0fc9c8aea661792db8eb3f873db2f21653ab956
Yassine Idrissi <yaidriss@c1r8p5.1337.ma>
2023-10-05 17:13:51 +0100

    fix all norm but still 3 modification in size of function to handl

--- math_stuff/put_texture.c
+++ math_stuff/put_texture.c
@@ -3,101 +3,114 @@
 /*                                                        :::      ::::::::   */
 /*   put_texture.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zouaraqa <zouaraqa@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 14:48:11 by zouaraqa          #+#    #+#             */
-/*   Updated: 2023/10/05 14:51:16 by zouaraqa         ###   ########.fr       */
+/*   Updated: 2023/10/05 17:13:12 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3D.h"
+#include "../cub3D.h"
 
-unsigned int pixel_value(t_cub3D *cb, double tx, t_putxt *p, int ty)
+unsigned int	pixel_value(t_cub3D *cb, double tx, t_putxt *p, int ty)
 {
-	int i;
+	int	i;
 
-	if(!p->hv)
+	if (!p->hv)
 	{
 		if (p->angle > M_PI)
 		{
 			i = cb->text[N].width * (tx) / T_S;
-			return(cb->text[N].img[ty][i]);
+			return (cb->text[N].img[ty][i]);
 		}
 		else
 		{
-			i = cb->text[S].width*(tx)/T_S;
-			return(cb->text[S].img[ty][cb->text[S].width -1 - i]);
+			i = cb->text[S].width * (tx) / T_S;
+			return (cb->text[S].img[ty][cb->text[S].width - 1 - i]);
 		}
 	}
 	else
 	{
 		if (p->angle > M_PI / 2 && p->angle < (3 * M_PI) / 2)
 		{
-			i = cb->text[WE].width*tx/T_S;
-			return(cb->text[WE].img[ty][cb->text[WE].width -1 - i]);
+			i = cb->text[WE].width * tx / T_S;
+			return (cb->text[WE].img[ty][cb->text[WE].width - 1 - i]);
 		}
 		else
 		{
-			i = cb->text[EA].width*tx/T_S;
-			return(cb->text[EA].img[ty][i]);
+			i = cb->text[EA].width * tx / T_S;
+			return (cb->text[EA].img[ty][i]);
 		}
 	}
 }
 
-int height_image(t_cub3D *cb ,int hv, double angle)
+int	height_image(t_cub3D *cb, int hv, double angle)
 {
-	if(!hv)
+	if (!hv)
 	{
 		if (angle > M_PI)
 			return (cb->text[N].height);
 		else
-			return(cb->text[S].height);
+			return (cb->text[S].height);
 	}
 	else
 	{
 		if (angle > M_PI / 2 && angle < (3 * M_PI) / 2)
-			return(cb->text[WE].height);
+			return (cb->text[WE].height);
 		else
-			return(cb->text[EA].height);
+			return (cb->text[EA].height);
 	}
 }
 
-void put_texture(t_cub3D *cb,t_putxt *p)
+void	put_texture(t_cub3D *cb, t_putxt *p)
 {
-	double ty_max = height_image(cb, p->hv, p->angle) - 1;
-	double ty = 0;
-	double 	ty_step = ty_max/p->line_lenth;
-	while (p->start_wall  < WINDOW_HEIGHT/2 +(p->line_lenth/2))
+	double	ty_max;
+	double	ty;
+	double	ty_step;
+
+	ty_max = height_image(cb, p->hv, p->angle) - 1;
+	ty = 0;
+	ty_step = ty_max / p->line_lenth;
+	while (p->start_wall < WINDOW_HEIGHT / 2 + (p->line_lenth / 2))
 	{
-		if (p->start_wall  >= 0 && p->start_wall < WINDOW_HEIGHT)
-			mlx_put_pixel(cb->img, p->i, p->start_wall , pixel_value(cb,(fmod(p->tx, T_S)), p, ty));
+		if (p->start_wall >= 0 && p->start_wall < WINDOW_HEIGHT)
+			mlx_put_pixel(cb->img, p->i, p->start_wall, pixel_value(cb,
+					(fmod(p->tx, T_S)), p, ty));
 		p->start_wall++;
 		ty += ty_step;
 	}
 }
 
-void double_free_int(unsigned int **map, int height, int width)
+void	double_free_int(unsigned int **map, int height, int width)
 {
-	int i = -1;
+	int	i;
+
+	i = -1;
 	while (++i < height)
 		free(map[i]);
 	free(map);
 }
 
-void draw_3d_image(t_cub3D *cb)
+void	draw_3d_image(t_cub3D *cb)
 {
-	mlx_texture_t *txt = cb->logo[M].txtr;
-	unsigned int **img = cb->logo[M].img;
+	mlx_texture_t	*txt;
+	unsigned int	**img;
+	int				i;
+	int				j;
+	int				width;
+	int				height;
 
-	int i = -1;
-	int j= -1;
-	int width = txt->width;
-	int height = txt->height;
-
+	txt = cb->logo[M].txtr;
+	img = cb->logo[M].img;
+	i = -1;
+	j = -1;
+	width = txt->width;
+	height = txt->height;
 	while (++i < height)
 	{
 		while (++j < width)
-			mlx_put_pixel(cb->img2,  j + 1150, i , cb->logo[cb->pos_logo].img[i][j]);
+			mlx_put_pixel(cb->img2, j + 1150, i,
+				cb->logo[cb->pos_logo].img[i][j]);
 		j = -1;
 	}
 	cb->t++;
@@ -106,6 +119,6 @@ void draw_3d_image(t_cub3D *cb)
 		cb->t = 0;
 		cb->pos_logo++;
 	}
-	if(cb->pos_logo == 5)
+	if (cb->pos_logo == 5)
 		cb->pos_logo = 0;
 }
--- math_stuff/txtur_colors.c
+++ math_stuff/txtur_colors.c
@@ -20,7 +20,7 @@ unsigned int	get_rgba(int r, int g, int b, int a)
 	return (c);
 }
 
-unsigned int *get_rgbas(uint8_t *pixels, int height, int width)
+unsigned int	*get_rgbas(uint8_t *pixels, int height, int width)
 {
 	unsigned int	*rgbas;
 	int				i;
@@ -29,25 +29,33 @@ unsigned int *get_rgbas(uint8_t *pixels, int height, int width)
 	i = 0;
 	j = 0;
 	rgbas = (unsigned int *)malloc(sizeof(unsigned int) * (height * width));
-	while(i < height * width * 4)
+	while (i < height * width * 4)
 	{
-		rgbas[j++] = get_rgba(pixels[i], pixels[i + 1], pixels[i + 2], pixels[i + 3]);
+		rgbas[j++] = get_rgba(pixels[i], pixels[i + 1], pixels[i + 2], pixels[i
+				+ 3]);
 		i += 4;
 	}
-	return(rgbas);
+	return (rgbas);
 }
 
-unsigned int **map_to_doublemap(t_cub3D *cb, mlx_texture_t *txtr)
+unsigned int	**map_to_doublemap(t_cub3D *cb, mlx_texture_t *txtr)
 {
-	int height = txtr->height;
-	int width = txtr->width;
-	unsigned int *map = get_rgbas(txtr->pixels, height, width);
-	int i = -1;
-	int j = -1;
-	int k = -1;
-	unsigned int **double_map;
-	double_map = malloc(sizeof(int *) *height);
-	while(++i < height)
+	int				height;
+	int				width;
+	unsigned int	*map;
+	int				i;
+	int				j;
+	int				k;
+	unsigned int	**double_map;
+
+	height = txtr->height;
+	width = txtr->width;
+	map = get_rgbas(txtr->pixels, height, width);
+	i = -1;
+	j = -1;
+	k = -1;
+	double_map = malloc(sizeof(int *) * height);
+	while (++i < height)
 		double_map[i] = malloc(sizeof(int) * width);
 	i = -1;
 	while (++i < height)
@@ -60,18 +68,19 @@ unsigned int **map_to_doublemap(t_cub3D *cb, mlx_texture_t *txtr)
 	return (double_map);
 }
 
-void load_text2(t_cub3D *cb)
+void	load_text2(t_cub3D *cb)
 {
-	int i;
-	
+	int	i;
+
 	i = -1;
 	cb->pos_logo = 0;
 	cb->t = 0;
-	while(++i < 4)
+	while (++i < 4)
 		if (cb->text[i].txtr)
-			cb->text[i].img = (unsigned int **)map_to_doublemap(cb, cb->text[i].txtr);
+			cb->text[i].img = (unsigned int **)map_to_doublemap(cb,
+					cb->text[i].txtr);
 	i = -1;
-	while(++i < 4)
+	while (++i < 4)
 	{
 		cb->text[i].width = cb->text[i].txtr->width;
 		cb->text[i].height = cb->text[i].txtr->height;
